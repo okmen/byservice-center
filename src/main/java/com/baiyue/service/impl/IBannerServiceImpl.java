5720027ec0a8ff9c22ab282a09080594676aae06
zhangyao <zhangyao@chudaokeji.com>
2018-09-19 15:41:07 +0800

    ww

--- src/main/java/com/baiyue/service/impl/IBannerServiceImpl.java
+++ src/main/java/com/baiyue/service/impl/IBannerServiceImpl.java
@@ -1,7 +1,6 @@
 package com.baiyue.service.impl;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.baiyue.services.IBannerService;
