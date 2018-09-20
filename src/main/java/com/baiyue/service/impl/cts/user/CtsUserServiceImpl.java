68a7ec15d6f6f3b51798d557b0d62257e5590d0b
zhangyao <zhangyao@chudaokeji.com>
2018-09-20 13:15:40 +0800

    da

--- src/main/java/com/baiyue/service/impl/cts/user/CtsUserServiceImpl.java
+++ src/main/java/com/baiyue/service/impl/cts/user/CtsUserServiceImpl.java
@@ -76,7 +76,7 @@ public class CtsUserServiceImpl implements ICtsUserService{
 				String token=UUID.randomUUID().toString();
 				System.out.println("token:"+token); 
 				try {
-//					RedisUtil.setObject(token, loginUser, 7200);
+					RedisUtil.setObject(token, loginUser, 7200);
 				} catch (Exception e) {
 					// TODO: handle exception
 					logger.error("redis问题："+e.getMessage()); 
